5958695a98ae4a4f743690ccaf572d58cb02fbca
felipe.nazareth <felipe.nazareth@itlean.com.br>
2019-11-11 01:18:35 -0300

    middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,18 @@
 import jwt from 'jsonwebtoken';
-import { promisify } from 'util';
+import {
+  promisify
+} from 'util';
 
+
+console.log("middleware")
 import authConfig from '../config/auth';
 
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).json({ error: 'token not provided' });
+    return res.status(401).json({
+      error: 'token not provided'
+    });
   }
   const [, token] = authHeader.split(' ');
   try {
@@ -16,6 +22,8 @@ export default async (req, res, next) => {
 
     return next();
   } catch (err) {
-    return res.status(401).json({ error: 'Token invalid' });
+    return res.status(401).json({
+      error: 'Token invalid'
+    });
   }
 };
